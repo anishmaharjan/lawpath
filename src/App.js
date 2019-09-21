cee2ab23f34b7adb2e3773a807713eed8f29d6c8
Anish Maharjan <dangol10@hotmail.com>
2019-09-22 01:34:20 +1000

    style and notification added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
-import './App.css';
+// import './App.css';
+import './style.css';
 import Request from './Service/api'
+import {NotificationContainer, NotificationManager} from 'react-notifications';
 
 class App extends React.Component {
+
+    /**
+    * Initializing states
+    */
     constructor(props) {
         super(props);
         this.state = {
@@ -12,18 +18,27 @@ class App extends React.Component {
         }
     }
 
+    /**
+    * Function that sets the value from form elements to state
+    */
     handleChange = (event) => {
         this.setState({[event.target.name]: event.target.value});
     };
 
+    /**
+    * Function that handles submit action
+    */
     handleSubmit = (event) => {
         let validationFlag = false;
 
+        /**
+        * Runs HTML5 validation in the forms
+        */
         if (!this.refs.auForm.checkValidity()) return;
 
         event.preventDefault();
 
-        //Api call
+        //Api call to AUPOST
         Request.get(`points/postcode/${this.state.postcode}`)
             .then(response => {
                 if (response.status === 200) {
@@ -48,67 +63,70 @@ class App extends React.Component {
             })
             .finally(() => {
                 if (!validationFlag) {
-                    console.log("error")
+                    console.log("Validation error")
+                    this.notify('info')
                 } else {
                     console.log("Successfully found")
+                    this.notify("success")
 
                 }
             })
     };
 
+    notify = (type) => {
+        return () => {
+          switch (type) {
+            case 'info':
+              NotificationManager.info('Info message');
+              break;
+            case 'success':
+              NotificationManager.success('Success message', 'Title here');
+              break;
+            case 'warning':
+              NotificationManager.warning('Warning message', 'Close after 3000ms', 3000);
+              break;
+            case 'error':
+              NotificationManager.error('Error message', 'Click me!', 5000, () => {
+                alert('callback');
+              });
+              break;
+          }
+      }
+    };
+
     render() {
         return (
+            <div className="container">
             <div>
-                <div className="container">
-                    <div className="screen">
-                        <div className="screen-header">
-                            <div className="screen-header-left">
-                                <div className="screen-header-button close"></div>
-                                <div className="screen-header-button maximize"></div>
-                                <div className="screen-header-button minimize"></div>
-                            </div>
-                            <div className="screen-header-right">
-                                <div className="screen-header-ellipsis"></div>
-                                <div className="screen-header-ellipsis"></div>
-                                <div className="screen-header-ellipsis"></div>
-                            </div>
+            <h3>Address Check Form</h3>
+            </div>
+                <div className="form-style">
+                    <form ref={"auForm"} >
+                        <div className="input-group">
+                            <label>
+                                Postcode: 
+                                <input type="number" name="postcode" max="9999" min="0" required onChange={this.handleChange} placeholder="eg. 2145 or 2111"/>
+                            </label>
                         </div>
-                        <div className="screen-body">
-                            <div className="screen-body-item left">
-                                <div className="app-title">
-                                    <span>Lawpath</span>
-                                </div>
-                                <div className="app-contact">SUBMITTED BY: A</div>
-                            </div>
-                            <div className="screen-body-item">
-                                <div className="app-form">
-                                    <form ref={"auForm"} className="background">
-                                        <div className="app-form-group">
-                                            <label>
-                                                Postcode:
-                                                <input className="app-form-control" type="number" name="postcode" max="9999" min="0" pattern="\d{4}" required onChange={this.handleChange}/>
-                                            </label>
-                                        </div>
-                                        <div className="app-form-group">
-                                            <label>
-                                                Suburb:
-                                                <input className="app-form-control" type="text" name="suburb" required onChange={this.handleChange}/>
-                                            </label>
-                                        </div>
-                                        <div className="app-form-group">
-                                            <label>
-                                                State:
-                                                <input className="app-form-control" type="text" name="state" required onChange={this.handleChange}/>
-                                            </label>
-                                        </div>
-                                        <div className="app-form-group buttons">
-                                            <button className="app-form-button" onClick={this.handleSubmit}>Submit</button>
-                                        </div>
-                                    </form>
-                                </div>
-                            </div>
+                        <div className="input-group">
+                            <label>
+                                Suburb: 
+                                <input type="text" name="suburb" required onChange={this.handleChange} placeholder="eg. Strathfield"/>
+                            </label>
                         </div>
-                    </div>
+                        <div className="input-group">
+                            <label>
+                                State: <small>( Abbreviation only)</small>
+                                <input type="text" name="state" required onChange={this.handleChange} maxLength="3"    pattern="[a-zA-Z]{2,3}" placeholder="eg. WA or NSW"/>
+                            </label>
+                        </div>
+                        <div className="input-group">
+                            <button onClick={this.handleSubmit}>Submit</button>
+                        </div>
+                    </form>
+                </div>
+                <div>
+
                 </div>
             </div>
         );
