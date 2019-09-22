3e6a0c8702fdce965fe075ee19b588812932a154
Anish Maharjan <dangol10@hotmail.com>
2019-09-22 11:09:57 +1000

    added validation

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import './style.css';
 import Request from './Service/api'
 import {NotificationContainer, NotificationManager} from 'react-notifications';
+import 'react-notifications/lib/notifications.css';
 
 class App extends React.Component {
 
@@ -17,6 +18,25 @@ class App extends React.Component {
             state: ''
         }
     }
+
+    /**
+    * Notiication module
+    */
+    notify = (type) => {
+        return () => {
+          switch (type) {
+            case 'success':
+              NotificationManager.success('Success', 'The address match');
+              break;
+            case 'warning':
+              NotificationManager.warning('Not valid', 'The postcode does not match state or suburb', 5000);
+              break;
+            case 'apiError':
+              NotificationManager.error('Error', 'Failed to retrieve data from AUPOST', 5000);
+              break;
+          }
+      }
+    };
 
     /**
     * Function that sets the value from form elements to state
@@ -59,40 +79,22 @@ class App extends React.Component {
                 }
             })
             .catch(error => {
+                this.notify('apiError')
                 console.log(error)
             })
             .finally(() => {
                 if (!validationFlag) {
                     console.log("Validation error")
-                    this.notify('info')
+                    this.notify('warning')
                 } else {
                     console.log("Successfully found")
-                    this.notify("success")
+                    this.notify("success");
 
                 }
             })
     };
 
-    notify = (type) => {
-        return () => {
-          switch (type) {
-            case 'info':
-              NotificationManager.info('Info message');
-              break;
-            case 'success':
-              NotificationManager.success('Success message', 'Title here');
-              break;
-            case 'warning':
-              NotificationManager.warning('Warning message', 'Close after 3000ms', 3000);
-              break;
-            case 'error':
-              NotificationManager.error('Error message', 'Click me!', 5000, () => {
-                alert('callback');
-              });
-              break;
-          }
-      }
-    };
+    
 
     render() {
         return (
@@ -128,6 +130,7 @@ class App extends React.Component {
                 <div>
 
                 </div>
+                <NotificationContainer/>
             </div>
         );
     }
